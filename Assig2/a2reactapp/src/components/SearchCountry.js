05ec500cad920ebe2d86c4b115c3be816e1dbfa3
duypnv2302 <duyduy2302@gmail.com>
2023-10-31 14:40:57 +1030

    improve sreach country

--- Assig2/a2reactapp/src/components/SearchCountry.js
+++ Assig2/a2reactapp/src/components/SearchCountry.js
@@ -1,55 +1,57 @@
-﻿import React, { useState } from 'react'
+﻿import React, { useState, useEffect } from 'react';
 import CountryCard from '../components/CountryCard';
-
 
 function SearchCountry() {
     const [cardData, setCardData] = useState([]);
     const [query, setQuery] = useState('');
 
-    React.useEffect(() => {
-        console.log("useEffect");
-        fetch(`http://localhost:5256/api/B_Countries/CountrySearch?searchText=${query}`)
-            .then(response => response.json())
-            .then(data => setCardData(data))
-            .catch(err => {
-                console.log(err);
-            });
-    }, [query])
+    useEffect(() => {
+        // Use async function within useEffect for fetching data
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`http://localhost:5256/api/B_Countries/CountrySearch?searchText=${query}`);
+                const data = await response.json();
+                setCardData(data);
+            } catch (err) {
+                console.error("Error fetching data: ", err);
+            }
+        };
 
-    function searchQuery() {
-        const value = document.querySelector('[name="searchText"]').value;
-        alert('value: ' + value);
+        // Call fetchData function
+        fetchData();
+    }, [query]); // Dependency on query
+
+    const searchQuery = (e) => {
+        e.preventDefault(); // Prevent default form submit behavior
+        const value = document.querySelector('[name="searchText"]').value.trim();
         setQuery(value);
     }
 
     return (
         <div id="cardListSearch">
-            <div className="row justify-content-start mb-3">
+            <form className="row justify-content-start mb-3" onSubmit={searchQuery}>
                 <div className="col-3">
                     <input type="text" name="searchText" className="form-control" placeholder="Type a country name" />
                 </div>
                 <div className="col text-left">
-                    <button type="button" className="btn btn-primary" onClick={searchQuery}>Search</button>
+                    <button type="submit" className="btn btn-primary">Search</button>
                 </div>
-            </div>
+            </form>
 
-
-            <div id="cardListSearch" className="row">
+            <div className="row">
                 {cardData.map((country) => (
                     <div key={country.countryId} className="col-md-4 mb-3">
-                        <div className="card" >
-                            <img src={country.imageUrl} className="card-img-top" alt={country.countryName} />
-                            <div className="card-body">
-                                <h5 className="card-title">{country.countryName}</h5>
-                            </div>
-                        </div>
+                        <CountryCard
+                            countryId={country.countryId}
+                            countryName={country.countryName}
+                            imageUrl={country.imageUrl}
+                        // other props if needed
+                        />
                     </div>
-                )
-                )
-                }
+                ))}
             </div>
         </div>
-    )
+    );
 }
 
 export default SearchCountry;
