b3a7842816cc78f776b155c663c5eeaa1a3e5c9d
duypnv2302 <duyduy2302@gmail.com>
2023-11-13 23:56:23 +1030

    start task 7, fetch data

--- Assig2/a2reactapp/src/components/AirQualityData.js
+++ Assig2/a2reactapp/src/components/AirQualityData.js
@@ -1,1 +1,15 @@
-﻿
+﻿import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+
+const CityAirQuality = () => {
+    const { cityId } = useParams();
+    const [cityAirQualityData, setCityAirQualityData] = useState(null);
+
+    useEffect(() => {
+        fetch(`http://localhost:5256/api/C_Cities/GetAirQualityData/${cityId}`)
+            .then(response => response.json())
+            .then(data => setCityAirQualityData(data))
+            .catch(error => console.error('Error:', error));
+    }, [cityId]);
+
+    
