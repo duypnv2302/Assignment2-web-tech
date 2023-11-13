c97fc83fb5b182fd2075142948ac2684ecd8aacf
duypnv2302 <duyduy2302@gmail.com>
2023-11-13 22:30:25 +1030

    start doing task 5 emission data - fetch data first

--- Assig2/a2reactapp/src/components/CityList.js
+++ Assig2/a2reactapp/src/components/CityList.js
@@ -1,1 +1,56 @@
-﻿
+﻿import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+
+const CitySearch = () => {
+    const { countryId } = useParams();
+    const [cities, setCities] = useState([]);
+    const [countryName, setCountryName] = useState('');
+    const [regionName, setRegionName] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        fetch(`http://localhost:5256/api/C_Cities/${countryId}`)
+            .then(response => response.json())
+            .then(data => {
+                setCities(data.cities);
+                setCountryName(data.countryName);
+                setRegionName(data.regionName);
+            })
+
+            .catch(error => console.error('Error:', error));
+    }, [countryId]);
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const filteredCities = cities.filter(city =>
+        city.cityName.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Search cities"
+                value={searchTerm}
+                onChange={handleSearchChange}
+            />
+
+            <div>
+                {filteredCities.map(city => (
+                    <div key={city.cityID}>
+                        <h3>{city.cityName}</h3>
+                        {city.airQualityYearRange && city.airQualityYearRange.length > 0 && (
+                            <Link to={`/city/${city.cityID}/airquality`}>View Air Quality Data</Link>
+                        )}
+                    </div>
+                ))}
+            </div>
+
+            <Link to="/country" className="btn btn-primary">Back to Country List</Link>
+        </div>
+    );
+};
+
+export default CitySearch;
--- Assig2/a2reactapp/src/components/EmissionData.js
+++ Assig2/a2reactapp/src/components/EmissionData.js
@@ -18,7 +18,7 @@ const CountryEmissionData = () => {
         fetch(`http://localhost:5256/api/B_Countries/CountryEmissionData/${countryId}`)
             .then(response => response.json())
             .then(data => {
-                setEmissionData(data.countryEmissions); // Adjust according to your API response
+                setEmissionData(data.countryEmissions); 
             })
             .catch(error => console.error('Error:', error));
     }, [countryId]);
@@ -31,65 +31,4 @@ const CountryEmissionData = () => {
             .catch(error => console.error('Error:', error));
     }, []);
 
-    // Element Filter Change Handler
-    const handleElementChange = (e) => {
-        setSelectedElement(e.target.value);
-    };
-
-    // Function to group emission data by year and element
-    const getGroupedEmissionData = () => {
-        let groupedData = {};
-        if (emissionData && Array.isArray(emissionData)) {
-            emissionData.forEach(emission => {
-                const year = emission.year;
-                if (!groupedData[year]) {
-                    groupedData[year] = [];
-                }
-                groupedData[year].push(emission);
-            });
-        }
-        return groupedData;
-    };
-
-    const groupedEmissionData = getGroupedEmissionData();
-
-    return (
-        <div className="container">
-            {countryData && (
-                <div>
-                    <h2>{countryData.countryName}</h2>
-                    <img src={countryData.imageUrl} alt={countryData.countryName} />
-                    <p>Region: {countryData.regionName}</p>
-                </div>
-            )}
-
-            <select value={selectedElement} onChange={handleElementChange}>
-                <option value="">Select an Element</option>
-                {elementList.map(element => (
-                    <option key={element.elementId} value={element.elementName}>
-                        {element.elementName}
-                    </option>
-                ))}
-            </select>
-
-            {Object.keys(groupedEmissionData).map(year => (
-                <div key={year}>
-                    <h3>Year: {year}</h3>
-                    {groupedEmissionData[year]
-                        .filter(data => selectedElement === '' || data.element === selectedElement)
-                        .map((data, index) => (
-                            <div key={index}>
-                                <p>Element: {data.element}</p>
-                                <p>Value: {data.value} {data.unit}</p>
-                            </div>
-                        ))
-                    }
-                </div>
-            ))}
-
-            <Link to="/country" className="btn btn-primary">Back to Country List</Link>
-        </div>
-    );
-};
-
-export default CountryEmissionData;
+    
